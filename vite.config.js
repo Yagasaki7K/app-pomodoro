c860b9288f29478020b26a66aeca65bf0ce2affa
Anderson Marlon <anderson18.marlon@gmail.com>
2024-05-01 13:32:38 -0300

    Ad: Added: PWA - 01-05 13:32

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
-  server: {
-    port: 3000,
-    host: '127.0.0.1',
-  }
+  plugins: [react(), svgr(), VitePWA({ registerType: 'autoUpdate' })],
 })
