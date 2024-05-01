41a996a7111f3adf217e687a83da2810dffcf45e
Anderson Marlon <anderson18.marlon@gmail.com>
2024-05-01 17:31:40 -0300

    Fix: Manifest on PWA - 01-05 17:31

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,27 @@ import million from 'million/compiler'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), million.vite({ auto: true }), VitePWA({
+  plugins: [react(), svgr(), million.vite({ auto: true }),
+  VitePWA({
     registerType: 'autoUpdate',
+    manifest: {
+      name: 'Appomodoro',
+      short_name: 'Pomodoro',
+      description: 'An intuitive timer built with React, following the Pomodoro technique',
+      theme_color: '#1c1b22',
+      icons: [
+        {
+          src: 'favicon.svg',
+          sizes: '192x192',
+          type: 'image/svg'
+        },
+        {
+          src: 'favicon.svg',
+          sizes: '512x512',
+          type: 'image/svg'
+        }
+      ]
+    },
     workbox: {
       clientsClaim: true,
       skipWaiting: true
