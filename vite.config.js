24c658c94b6dd4f1739c49e3761bfba2f2cfb7d9
Anderson Marlon <anderson18.marlon@gmail.com>
2024-05-01 17:26:32 -0300

    AddAdded: Million - 01-05 17:26

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import { VitePWA } from 'vite-plugin-pwa'
+import million from 'million/compiler'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), VitePWA({
+  plugins: [react(), svgr(), million.vite({ auto: true }), VitePWA({
     registerType: 'autoUpdate',
     workbox: {
       clientsClaim: true,
