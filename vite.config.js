a3f8ee5cf4c917da558b0d4a862ce023ff228d94
Anderson Marlon <anderson18.marlon@gmail.com>
2024-11-03 21:55:08 -0300

    Update: PWA - 03-11 21:55

--- vite.config.js
+++ vite.config.js
@@ -6,33 +6,60 @@ import million from 'million/compiler'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), million.vite({ auto: true }),
-  VitePWA({
-    registerType: 'autoUpdate',
-    manifest: {
-      name: 'Appomodoro',
-      short_name: 'Pomodoro',
-      description: 'An intuitive timer built with React, following the Pomodoro technique',
-      theme_color: '#1c1b22',
-      icons: [
-        {
-          src: 'favicon.svg',
-          sizes: '192x192',
-          type: 'image/svg'
-        },
-        {
-          src: 'favicon.svg',
-          sizes: '512x512',
-          type: 'image/svg'
-        }
-      ]
-    },
-    workbox: {
-      clientsClaim: true,
-      skipWaiting: true
-    }
-    // devOptions: {
-    //   enabled: true
-    // }
-  })],
+  plugins: [
+    react(),
+    svgr(),
+    million.vite({ auto: true }),
+    VitePWA({
+      registerType: 'autoUpdate', // Auto-update the Service Worker
+      injectRegister: 'auto', // Automatically injects the SW registration into HTML
+      manifest: {
+        name: 'Appomodoro',
+        short_name: 'Pomodoro',
+        description: 'An intuitive timer built with React, following the Pomodoro technique',
+        theme_color: '#1c1b22',
+        icons: [
+          {
+            src: 'favicon.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml'
+          },
+          {
+            src: 'favicon.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml'
+          }
+        ]
+      },
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+        runtimeCaching: [
+          {
+            urlPattern: /\.(?:png|jpg|jpeg|svg|gif|mp3)$/, // Cache images and mp3 files
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'asset-cache',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 30 * 24 * 60 * 60 // 30 days
+              }
+            }
+          },
+          {
+            urlPattern: /^https:\/\/appomodoro\.vercel\.app\//,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'runtime-cache',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 30 * 24 * 60 * 60 // 30 days
+              }
+            }
+          }
+        ]
+      },
+      includeAssets: ['favicon.svg', 'rain.mp3', 'alarm.mp3'] // Ensure sound files are cached
+    })
+  ]
 })
